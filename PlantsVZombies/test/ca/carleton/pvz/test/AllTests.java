6a8a658a6f3f576cf0035a7a3485496559c7961a
SameedMohammed <SameedMohammed@192.168.0.13>
2018-11-16 18:41:01 -0500

    suite file was configured incorrectly, recreated the suite file

---
+++ PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
@@ -0,0 +1,12 @@
+package ca.carleton.pvz.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ActorTest.class, CooldownManagerTest.class, LevelOneTest.class, LevelTest.class, PeaShooterTest.class,
+		PlantTest.class, SunflowerTest.class, WaveTest.class, WorldTest.class, ZombieTest.class })
+public class AllTests {
+
+}
