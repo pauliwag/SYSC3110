539e116a6616159c1dd87f5e814c956fe8dd0a01
SameedMohammed <SameedMohammed@192.168.0.13>
2018-11-16 22:11:51 -0500

    Finished PeashooterTest entirely

--- PlantsVZombies/test/ca/carleton/pvz/test/PeaShooterTest.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/PeaShooterTest.java
@@ -2,33 +2,55 @@ package ca.carleton.pvz.test;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.awt.Point;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ca.carleton.pvz.actor.PeaShooter;
+import ca.carleton.pvz.actor.Zombie;
+import ca.carleton.pvz.level.Level;
+import ca.carleton.pvz.level.LevelOne;
+
 public class PeaShooterTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
+	private Level testLevel;
+	private PeaShooter testPeaShooter;
+	private Zombie testZombie;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		testPeaShooter = new PeaShooter();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHits() {
+		assertEquals(0, testPeaShooter.getHits());
+		testPeaShooter.addHit();
+		assertEquals(1, testPeaShooter.getHits());
+		testPeaShooter.addHit();
+		testPeaShooter.newTurn();
+		assertEquals(0, testPeaShooter.getHits());
 	}
+	
+	@Test
+	public void testShoot() {
+		testLevel = new LevelOne();
+		testZombie = new Zombie();
+		
+		testLevel.placeActor(testPeaShooter, new Point(0,2));
+		testLevel.placeActor(testZombie, new Point(4,2));
+		assertEquals(600, testZombie.getHealth());
+		
+		PeaShooter.shootZombies(testLevel);
+		assertEquals(200, testZombie.getHealth());
+		
+		PeaShooter.shootZombies(testLevel);
+		assertEquals(-200, testZombie.getHealth());
+	}
+	
+	// tearDown() is not necessary here, as garbage collection of objects
+	// after the test class concludes. Other things that consume system
+	// resources may need tearDown() however (like GUI).
 
 }
