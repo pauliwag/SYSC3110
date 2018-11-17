533210f25bfa2d72a98232ba007fc19b54345bcf
SameedMohammed <SameedMohammed@192.168.0.13>
2018-11-16 22:45:04 -0500

    Finished WaveTest.java entirely, added it to the suite test class

--- PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ ActorTest.class, CooldownManagerTest.class, LevelOneTest.class, LevelTest.class, PeaShooterTest.class,
-		PlantTest.class, SunflowerTest.class/*, WaveTest.class, WorldTest.class, ZombieTest.class*/ })
+		PlantTest.class, SunflowerTest.class, WaveTest.class/*, WorldTest.class, ZombieTest.class*/ })
 
 public class AllTests {
 
--- PlantsVZombies/test/ca/carleton/pvz/test/WaveTest.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/WaveTest.java
@@ -8,27 +8,49 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ca.carleton.pvz.actor.Zombie;
+import ca.carleton.pvz.level.Level;
+import ca.carleton.pvz.level.LevelOne;
+import ca.carleton.pvz.level.Wave;
+
 public class WaveTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	Wave testWave;
+	LevelOne testLevel;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		testWave = new Wave(1, 3);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetters() {
+		assertEquals(1, testWave.getNum());
+		assertEquals(3, testWave.getRemainingZombies());
+		
+		testWave.setWaveNumber(2);
+		assertEquals (2, testWave.getNum());
+		
+		testWave.setRemainingZombies(5);
+		assertEquals(5, testWave.getRemainingZombies());
 	}
+	
+	@Test
+	public void testSpawnZombie() {
+		testLevel = new LevelOne();
+		Wave.spawnZombieOnLevel(testLevel);
+		
+		boolean flag = false;
+		for (int i = 0; i < 5; i++) {
+			if (testLevel.getCell(4, i) instanceof Zombie) {
+				flag = true;
+			}
+		}
+		assertTrue(flag);
+	}
+	
+	// tearDown() is not necessary here, as garbage collection of objects
+	// after the test class concludes. Other things that consume system
+	// resources may need tearDown() however (like GUI).
 
 }
