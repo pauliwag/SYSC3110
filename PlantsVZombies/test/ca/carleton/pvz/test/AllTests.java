082cfa98f035571b478cf0e3c0151b933d237b2a
SameedMohammed <SameedMohammed@192.168.0.10>
2018-11-25 03:16:29 -0500

    Fixed AllTests as a whole, so that it includes missing test cases.

--- PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ ActorTest.class, CooldownManagerTest.class, LevelOneTest.class, LevelTest.class, NormalPeaShooterTest.class,
-		PlantTest.class, SunflowerTest.class, WaveTest.class, WorldTest.class, ZombieTest.class, PeaShooterTest.class, GatlingPeaShooterTest.class })
+		PlantTest.class, SunflowerTest.class, WaveTest.class, WorldTest.class, ZombieTest.class, PeaShooterTest.class, GatlingPeaShooterTest.class, 
+		FastZombieTest.class, NormalZombieTest.class, ShieldZombieTest.class, })
 
 public class AllTests {
 
