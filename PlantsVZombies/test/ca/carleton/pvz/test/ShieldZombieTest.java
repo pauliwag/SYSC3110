3b9a21d0c71785138863f5c01b7fe070806d4521
abdil <abdil@192.168.0.12>
2018-11-25 01:56:40 -0500

    Added Test class for Shield Zombie type

---
+++ PlantsVZombies/test/ca/carleton/pvz/test/ShieldZombieTest.java
@@ -0,0 +1,48 @@
+package ca.carleton.pvz.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.carleton.pvz.actor.Zombie;
+import ca.carleton.pvz.actor.ShieldZombie;
+import javafx.scene.image.Image;
+
+public class ShieldZombieTest {
+
+	Zombie testZombie;
+
+	@Before
+	public void setUp() throws Exception {
+		testZombie = new ShieldZombie();
+
+	}
+
+	/**
+	 * Tests the basic getter methods of the ShieldZombie class Subclasses of Zombie
+	 * will be used to ensure methods are working as intended
+	 * 
+	 * @result All getter methods should be working as intended and returns the
+	 *         correct values
+	 */
+	@Test
+	public void testGetters() {
+		assertTrue(testZombie instanceof Zombie);
+		assertEquals(700, testZombie.getHealth());
+		assertEquals(1, testZombie.getSpeed());
+
+	}
+
+	/**
+	 * Tests the Sprite method for ShieldZombie
+	 * 
+	 * @result The expected type for the Object returned by this method should be an
+	 *         Image type
+	 */
+	@Test
+	public void testSprite() {
+		assertTrue(testZombie.getSprite() instanceof Image);
+	}
+
+}
