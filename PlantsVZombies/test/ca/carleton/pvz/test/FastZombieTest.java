bc3dfc2b4d50b94593d562375973f5ffdc5b1cd8
abdil <abdil@192.168.0.12>
2018-11-25 01:54:55 -0500

    Fixed Formatting

--- PlantsVZombies/test/ca/carleton/pvz/test/FastZombieTest.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/FastZombieTest.java
@@ -20,7 +20,7 @@ public class FastZombieTest {
 	}
 
 	/**
-	 * Tests the basic getter methods of the NormalZombie class Subclasses of Zombie
+	 * Tests the basic getter methods of the FastZombie class Subclasses of Zombie
 	 * will be used to ensure methods are working as intended
 	 * 
 	 * @result All getter methods should be working as intended and returns the
@@ -35,7 +35,7 @@ public class FastZombieTest {
 	}
 
 	/**
-	 * Tests the Sprite method for NormalZombie
+	 * Tests the Sprite method for FastZombie
 	 * 
 	 * @result The expected type for the Object returned by this method should be an
 	 *         Image type
