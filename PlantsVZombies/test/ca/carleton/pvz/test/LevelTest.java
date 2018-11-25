192857b078eeb0f51a527d6d40c37f998829a550
abdil <abdil@192.168.0.12>
2018-11-25 02:10:37 -0500

    Modified LevelTest class to reflect iteration 3 changes

--- PlantsVZombies/test/ca/carleton/pvz/test/LevelTest.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/LevelTest.java
@@ -8,31 +8,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ca.carleton.pvz.level.LevelOne;
-import ca.carleton.pvz.actor.Actor;
+
+import ca.carleton.pvz.actor.NormalZombie;
 import ca.carleton.pvz.level.Level;
 
 public class LevelTest {
 
 	private Level testLevel;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		testLevel = new LevelOne(); //runs a "super." which fills in values of "Level One", 5, 5, 500.
+		testLevel = new LevelOne(); // runs a "super." which fills in values of "Level One", 5, 5, 500.
 	}
 
 	/**
 	 * Create a valid account.
-	 * @result Account will be persisted without any errors,
-	 *         and Account.getId() will no longer be <code>null</code>
+	 * 
+	 * @result Account will be persisted without any errors, and Account.getId()
+	 *         will no longer be <code>null</code>
 	 */
 	@Test
 	public void testCompletion() {
 		assertFalse(testLevel.isHeadWaveEmpty());
 		assertFalse(testLevel.isBeat());
 	}
-	
+
 	/**
 	 * Tests the turns functionality of the Level class
+	 * 
 	 * @result All turns increment correctly and as expected
 	 */
 	@Test
@@ -40,53 +43,49 @@ public class LevelTest {
 		assertEquals(0, testLevel.getTurn());
 		testLevel.incTurn();
 		assertEquals(1, testLevel.getTurn());
-		assertEquals(0, testLevel.getPrevTurn());
 	}
-	
+
 	/**
 	 * Tests the cell functionality of the Level class (square on the board)
-	 * @result All cells place and "get" as intended, whether there is an Actor on the cell or not
+	 * 
+	 * @result All cells place and "get" as intended, whether there is an Actor on
+	 *         the cell or not
 	 */
 	@Test
 	public void testCell() {
-		//CELL TESTING
-		assertEquals(null, testLevel.getCell(2,2));
-		testLevel.placeActor(new Actor(), new Point(2, 2));
+		// CELL TESTING
+		assertEquals(null, testLevel.getCell(2, 2));
+		testLevel.placeActor(new NormalZombie(), new Point(2, 2));
 		assertNotEquals(null, testLevel.getCell(2, 2));
 	}
-	
-	/**
-	 * Tests the dimensions functionality of the board in Level
-	 * @result the Dimension getters work as intended (it's only expecting 5 in this case because the default size is 5x5)
-	 */
-	@Test
-	public void testDimension() {
-		assertEquals(5, (int)testLevel.getDimension().getHeight());
-		assertEquals(5, (int)testLevel.getDimension().getWidth());
-	}
-	
+
 	/**
 	 * Testing the name of Level
+	 * 
 	 * @result The name of testLevelOne should be "Level 1"
 	 */
 	@Test
 	public void testName() {
 		assertEquals("Level 1", testLevel.getLevelName());
 	}
-	
+
 	/**
 	 * Testing the isPointValid() method
-	 * @result The method works as intended, and returns true and false accordingly. Both are tested extensively.
+	 * 
+	 * @result The method works as intended, and returns true and false accordingly.
+	 *         Both are tested extensively.
 	 */
 	@Test
 	public void testIsPointValid() {
 		assertTrue(testLevel.isPointValid(new Point(3, 1)));
 		assertFalse(testLevel.isPointValid(new Point(8, 8)));
 	}
-	
+
 	/**
 	 * Tests the sunpoints mechanic of Level
-	 * @result All the sunpoints variations behave as intended, including adding or subtracting sunpoints
+	 * 
+	 * @result All the sunpoints variations behave as intended, including adding or
+	 *         subtracting sunpoints
 	 */
 	@Test
 	public void testSunpoints() {
