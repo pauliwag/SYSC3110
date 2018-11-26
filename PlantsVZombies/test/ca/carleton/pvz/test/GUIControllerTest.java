f320027f14ea652b0fc6584058784cb5141134ee
abdil <abdil@192.168.0.16>
2018-11-25 19:53:38 -0500

    Created GUIController class, tests need to be filled in

---
+++ PlantsVZombies/test/ca/carleton/pvz/test/GUIControllerTest.java
@@ -0,0 +1,29 @@
+package ca.carleton.pvz.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.carleton.pvz.PlantsVZombies;
+import ca.carleton.pvz.actor.Plant;
+import ca.carleton.pvz.gui.GUIController;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
+public class GUIControllerTest {
+	private PlantsVZombies game;
+	private GUIController controller;
+	private Plant testPlant;
+
+	@Before
+	public void setUp() throws Exception {
+		// testActor = new NormalZombie();
+		game = new PlantsVZombies();
+		controller = new GUIController();
+		controller.initialize();
+	}
+	
+
+}
