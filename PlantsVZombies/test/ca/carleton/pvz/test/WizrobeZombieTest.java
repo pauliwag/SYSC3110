9777f0c71128f2f96558ba10ec2524362b9e9743
SameedMohammed <SameedMohammed@192.168.0.17>
2018-12-06 22:20:35 -0500

    added test cases for wizrobe and teleporting...

---
+++ PlantsVZombies/test/ca/carleton/pvz/test/WizrobeZombieTest.java
@@ -0,0 +1,52 @@
+package ca.carleton.pvz.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.carleton.pvz.actor.Zombie;
+import ca.carleton.pvz.level.Level.Terrain;
+import ca.carleton.pvz.actor.WizrobeZombie;
+import javafx.scene.image.Image;
+
+public class WizrobeZombieTest {
+
+	Zombie testZombie;
+
+	@Before
+	public void setUp() throws Exception {
+		testZombie = new WizrobeZombie();
+
+	}
+
+	/**
+	 * Tests the basic getter methods of the WizrobeZombie class Subclasses of Zombie
+	 * will be used to ensure methods are working as intended
+	 * 
+	 * @result All getter methods should be working as intended and returns the
+	 *         correct values
+	 */
+	@Test
+	public void testGetters() {
+		assertTrue(testZombie instanceof Zombie);
+		assertEquals(500, testZombie.getHealth());
+		assertEquals(1, testZombie.getSpeed());
+		testZombie.setHealth(testZombie.getHealth() - 0);
+		assertFalse(testZombie.getHealth() == 0);
+
+	}
+
+	/**
+	 * Tests the Sprite method for WizrobeZombie
+	 * 
+	 * @result The expected type for the Object returned by this method should be an
+	 *         Image type
+	 */
+	@Test
+	public void testSprite() {
+		assertFalse(testZombie.getSprite(Terrain.GRASS) == null);
+		assertTrue(testZombie.getSprite(Terrain.GRASS) instanceof Image);
+	}
+
+}
