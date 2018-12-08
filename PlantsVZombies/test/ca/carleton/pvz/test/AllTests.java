4df845768535024358083c5168603204f01521c5
SameedMohammed <SameedMohammed@192.168.0.17>
2018-12-07 19:08:50 -0500

    UndoRedoTests working! Added a few utility methods to Level

--- PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
+++ PlantsVZombies/test/ca/carleton/pvz/test/AllTests.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ ActorTest.class, CooldownManagerTest.class, LevelOneTest.class, LevelTest.class, NormalPeaShooterTest.class,
 		PlantTest.class, SunflowerTest.class, WaveTest.class, WorldTest.class, ZombieTest.class, PeaShooterTest.class, 
 		GatlingPeaShooterTest.class, FastZombieTest.class, NormalZombieTest.class, ShieldZombieTest.class, GUIControllerTest.class, 
-		FootballZombieTest.class, BossZombieTest.class, GigaZombieTest.class, WizrobeZombieTest.class, TeleportingZombieTest.class})
+		FootballZombieTest.class, BossZombieTest.class, GigaZombieTest.class, WizrobeZombieTest.class, TeleportingZombieTest.class,
+		UndoRedoTest.class})
 
 public class AllTests {
 
